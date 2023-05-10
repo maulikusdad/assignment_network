85a0d24bb57ea094ca2fda48a165a93fee9273bf
maulikusdad <maulikpatel1614@gmail.com>
2023-05-10 16:15:53 +0530

    assignment completed

--- assignment.c
+++ assignment.c
@@ -3,10 +3,9 @@
 
 #define MAX_N 100000
 
-int parent[MAX_N];  // array to store the parent of each workstation
-int rank[MAX_N];    // array to store the rank of each workstation
+int parent[MAX_N];  
+int rank[MAX_N];    
 
-// find the parent of a workstation using path compression
 int find(int x) {
     if (parent[x] != x) {
         parent[x] = find(parent[x]);
@@ -14,7 +13,7 @@ int find(int x) {
     return parent[x];
 }
 
-// merge two sets of workstations using union-by-rank heuristic
+
 void merge(int x, int y) {
     int px = find(x);
     int py = find(y);
@@ -32,27 +31,27 @@ void merge(int x, int y) {
 
 int makeConnected(int n, int** connections, int connectionsSize, int* connectionsColSize){
     if (connectionsSize < n - 1) {
-        return -1;  // not enough connections to connect all workstations
+        return -1;  
     }
-    // initialize each workstation as a separate set
+    
     for (int i = 0; i < n; i++) {
         parent[i] = i;
         rank[i] = 0;
     }
-    // merge sets of workstations connected by each cable
+    
     for (int i = 0; i < connectionsSize; i++) {
         int x = connections[i][0];
         int y = connections[i][1];
         merge(x, y);
     }
-    // count the number of sets (i.e., connected components)
+   
     int count = 0;
     for (int i = 0; i < n; i++) {
         if (parent[i] == i) {
             count++;
         }
     }
-    // the number of extra cables needed is equal to the number of sets minus one
+    
     return count - 1;
 }
 
@@ -63,6 +62,6 @@ int main() {
     int* connectionsPtr[3] = {connections[0], connections[1], connections[2]};
     int connectionsColSize[3] = {2, 2, 2};
     int minCables = makeConnected(n, connectionsPtr, connectionsSize, connectionsColSize);
-    printf("%d\n", minCables);  // output: 1
+    printf("%d\n", minCables); 
     return 0;
 }
